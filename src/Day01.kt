57a086ca015f5663625b9d0dbd1a8bd7e1768237
Spiri <spiri16@protonmail.com>
2021-12-01 23:40:47 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,22 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var res = 0
+        for(i in 1 until input.size){
+            if(input[i].toInt()>input[i-1].toInt()){
+                res++
+            }
+        }
+        return res
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var res = 0
+        for(i in 3 until input.size){
+            if(input[i].toInt()>input[i-3].toInt()){
+                res++
+            }
+        }
+        return res
     }
 
     // test if implementation meets criteria from the description, like:
