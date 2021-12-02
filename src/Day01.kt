92ab580f57eccaa1082c305079488136c4869630
Spiri <spiri16@protonmail.com>
2021-12-02 01:53:41 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -19,10 +19,6 @@ fun main() {
         return res
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
